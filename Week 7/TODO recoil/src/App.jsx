e82b8aaca4003a5a43b06bf9a2a1d86c8fe57feb
Sharma17Deepak <sharmadeepoo30@gmail.com>
2026-06-08 11:47:48 +0530

    todo assignment

---
+++ Week 7/TODO recoil/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <RecoilRoot>
+        <Todo/>
+      </RecoilRoot>
+    </>
+  )
+}
+
+function Todo(){
+}
+
+export default App
